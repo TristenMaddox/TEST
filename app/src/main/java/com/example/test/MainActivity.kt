c593fa7afb22310581ddd383ea23ac5f750b867d
maddotri000 <maddotri000@warren.k12.in.us>
2024-05-14 10:54:17 -0400

    comm

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,22 +1,72 @@
 package com.example.test
 
-import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.test.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    @Overide
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class MainActivity  : AppCompatActivity()
+{
+    enum class Turn{
+        NOUGHT,
+        CROSS
     }
+
+    private var firstTurn = Turn.CROSS
+    private var currentTurn = Turn.CROSS
+
+    private var boardList = mutableListOf<Button>()
+
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBoard()
+}
+    private fun initBoard()
+    {
+        boardList.add(binding.a1)
+        boardList.add(binding.a2)
+        boardList.add(binding.a3)
+        boardList.add(binding.b1)
+        boardList.add(binding.b2)
+        boardList.add(binding.b3)
+        boardList.add(binding.c1)
+        boardList.add(binding.c2)
+        boardList.add(binding.c3)
+
+    }
+    fun boardTapped(view: View)
+    {
+        if(view !is Button)
+            return
+        addToBoard(view)
+    }
+    private fun addToBoard(button: Button)
+    {
+       if(button.text != "")
+        return
+
+        if(currentTurn == Turn.NOUGHT)
+            {
+            button.text = NOUGHT
+            currentTurn = Turn.CROSS
+        }
+           else if(currentTurn == Turn.CROSS)
+            {
+                button.text = CROSS
+                currentTurn = Turn.NOUGHT
+
+}
+
+    companion object
+    {
+    setTurnLabel()
+    }
+const val NOUGHT = "0"
+const val CROSS = "0"
+}
 }
