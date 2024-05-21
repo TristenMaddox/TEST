ff25fc9550180055729ff673a12cab98933e5a58
maddotri000 <maddotri000@warren.k12.in.us>
2024-05-21 10:20:45 -0400

    com.

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.test
 
+import android.icu.text.CaseMap.Title
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import com.example.test.databinding.ActivityMainBinding
 
-class MainActivity  : AppCompatActivity()
-{
-    enum class Turn{
+class MainActivity  : AppCompatActivity() {
+    enum class Turn {
         NOUGHT,
         CROSS
     }
@@ -18,16 +18,15 @@ class MainActivity  : AppCompatActivity()
 
     private var boardList = mutableListOf<Button>()
 
-    private lateinit var binding : ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
-         super.onCreate(savedInstanceState)
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initBoard()
-}
-    private fun initBoard()
-    {
+    }
+
+    private fun initBoard() {
         boardList.add(binding.a1)
         boardList.add(binding.a2)
         boardList.add(binding.a3)
@@ -39,34 +38,69 @@ class MainActivity  : AppCompatActivity()
         boardList.add(binding.c3)
 
     }
-    fun boardTapped(view: View)
-    {
-        if(view !is Button)
+
+    fun boardTapped(view: View) {
+        if (view !is Button)
             return
         addToBoard(view)
-    }
-    private fun addToBoard(button: Button)
-    {
-       if(button.text != "")
-        return
 
-        if(currentTurn == Turn.NOUGHT)
-            {
+        if (fullBoard())
+        {
+         result("Draw")
+        }
+
+    }
+    private fun result(title: String)
+    {
+        AlertDialog.Builder(context:this)
+        .setTitle(title)
+        .setPositiveButton("Reset")
+        {_,_ ->
+            resetBoard()
+        }
+    }
+    private fun fullBoard(): Boolean
+    {
+        for (button in boardList)
+        {
+            if(button.text == "")
+                return false
+    }
+        return true
+}
+    private fun addToBoard(button: Button) {
+        if (button.text != "")
+            return
+
+        if (currentTurn == Turn.NOUGHT)
+        {
             button.text = NOUGHT
             currentTurn = Turn.CROSS
         }
-           else if(currentTurn == Turn.CROSS)
-            {
-                button.text = CROSS
-                currentTurn = Turn.NOUGHT
+         else if (currentTurn == Turn.CROSS)
+        {
+            button.text = CROSS
+            currentTurn = Turn.NOUGHT
 
-}
+        }
+        setTurnLabel()
+    }
 
+    private fun setTurnLabel()
+    {
+        var turnText = ""
+        if(currentTurn == Turn.CROSS)
+            turnText = "Turn $CROSS"
+       else if(currentTurn == Turn.NOUGHT)
+            turnText = "Turn $CROSS"
+
+        binding.textTV.text = turnText
+    }
     companion object
     {
-    setTurnLabel()
-    }
-const val NOUGHT = "0"
-const val CROSS = "0"
+
+
+const val NOUGHT = "O"
+const val CROSS = "X"
 }
 }
